018a89d1a72c19ae769b4625a356a573e4c5fe6a
Kakajan-12 <83004880+Kakajan-12@users.noreply.github.com>
2025-10-25 02:24:03 +0500

    disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,22 @@
 module.exports = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'api.koshibakery.com',
-    //             pathname: '/uploads/**',
-    //         },
-    //     ],
-    // },
+    unoptimized: true,
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3001',
+                protocol: 'https',
+                hostname: 'api.koshibakery.com',
                 pathname: '/uploads/**',
             },
         ],
     },
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             port: '3001',
+    //             pathname: '/uploads/**',
+    //         },
+    //     ],
+    // },
 };
